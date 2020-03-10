ac3521676a463e25a79090f406c5f42274d3cc5e
Sarah Lee <scrislee@gmail.com>
2020-03-10 12:57:02 -0400

    Moved keys

---
+++ assignments/keys/InClass_Ttest_Key.R
@@ -0,0 +1,16 @@
+### Four t-test scenarios given in class, due Monday 2020-03-09
+
+# Clean up the working environment
+rm(list = ls())
+
+### Install and load packages ####
+
+if(!require(Rmisc)){install.packages("Rmisc")}
+if(!require(DescTools)){install.packages("DescTools")}
+if(!require(boot)){install.packages("boot")}
+if(!require(rcompanion)){install.packages("rcompanion")}
+if(!require(summarytools)){install.packages("summarytools")}
+if(!require(tidyverse)){install.packages("tidyverse")}
+
+# Check for updates
+tidyverse_update()
